e0e52dfd01e4e1a414ec3c6ca9c240014367868f
tesonkim <teson0228@gmail.com>
2025-06-08 17:28:34 +0900

    add D2 diagram for chatbot applications structure

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## install D2
+# With --dry-run the install script will print the commands it will use
+# to install without actually installing so you know what it's going to do.
+curl -fsSL https://d2lang.com/install.sh | sh -s -- --dry-run
+# If things look good, install for real.
+curl -fsSL https://d2lang.com/install.sh | sh -s --
+
